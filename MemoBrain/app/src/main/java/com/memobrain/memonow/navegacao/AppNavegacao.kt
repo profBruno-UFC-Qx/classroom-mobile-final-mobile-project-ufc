02522478cc26924419767a9700500ae76b807aea
AllysonAugusto <allysonfn9@hotmail.com>
2026-06-04 05:15:42 -0300

    refactor: organiza navegacao do app em arquivos separados

--- MemoBrain/app/src/main/java/com/memobrain/memonow/navegacao/AppNavegacao.kt
+++ MemoBrain/app/src/main/java/com/memobrain/memonow/navegacao/AppNavegacao.kt
@@ -1,2 +1,36 @@
 package com.memobrain.memonow.navegacao
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.memobrain.memonow.features.login.LoginTela
+import com.memobrain.memonow.features.login.TelaInicial
+import com.memobrain.memonow.features.registrar.RegistrarTela
+
+@Composable
+fun AppNavegacao(){
+    var telaAtual by remember {mutableStateOf(RotasTelas.INICIAL)}
+
+    when (telaAtual){
+        RotasTelas.INICIAL -> {
+            TelaInicial(
+                entrar = {telaAtual = RotasTelas.LOGIN},
+                registrar = {telaAtual = RotasTelas.REGISTRAR}
+            )
+        }
+
+        RotasTelas.LOGIN -> {
+            LoginTela(
+                registrar = {telaAtual = RotasTelas.REGISTRAR}
+            )
+        }
+
+        RotasTelas.REGISTRAR -> {
+            RegistrarTela()
+        }
+
+    }
+
+}
---
+++ MemoBrain/app/src/main/java/com/memobrain/memonow/navegacao/rotas_telas.kt
@@ -0,0 +1,7 @@
+package com.memobrain.memonow.navegacao
+
+enum class RotasTelas{
+    INICIAL,
+    LOGIN,
+    REGISTRAR
+}
