418c650ed50956aef4b81989a684ecbe1d5b8ef8
AllysonAugusto <allysonfn9@hotmail.com>
2026-06-04 04:18:24 -0300

    feat: adiciona controle de navegacao entre tela inicial, login e cadastro

--- MemoBrain/app/src/main/java/com/memobrain/memonow/MainActivity.kt
+++ MemoBrain/app/src/main/java/com/memobrain/memonow/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.memobrain.memonow.features.login.LoginTela
 import com.memobrain.memonow.features.login.TelaInicial
+import com.memobrain.memonow.features.registrar.RegistrarTela
 import com.memobrain.memonow.ui.tema.MemonowTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,14 +28,35 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+enum class TelaAtual{
+    INICIAL,
+    LOGIN,
+    REGISTRAR
+}
+
 @Composable
 fun App() {
-    Scaffold { padding ->
+    var telaAtual by remember {mutableStateOf(TelaAtual.INICIAL)}
 
-        TelaInicial(modifier = Modifier.padding(padding))
-        //LoginTela(modifier = Modifier.padding(padding))
-        //CadernosTelas(modifier = Modifier.padding(padding))
-        //CadernosTelas2(modifier = Modifier.padding(padding))
-        //RegistrarTela(modifier = Modifier.padding(padding))
+    when(telaAtual) {
+        TelaAtual.INICIAL -> {
+            TelaInicial(
+                entrar = {telaAtual = TelaAtual.LOGIN},
+                registrar = {telaAtual = TelaAtual.REGISTRAR}
+            )
+        }
+
+
+        TelaAtual.LOGIN -> {
+            LoginTela(
+                registrar = {telaAtual = TelaAtual.REGISTRAR}
+            )
+        }
+
+        TelaAtual.REGISTRAR -> {
+            RegistrarTela()
+        }
     }
+
 }
+
