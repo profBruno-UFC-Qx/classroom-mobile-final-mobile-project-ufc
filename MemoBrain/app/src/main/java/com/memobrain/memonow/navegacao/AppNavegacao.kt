5e8e622a4fdd80b30da261b687f20a20612614cc
wiliene <willienes49@gmail.com>
2026-06-16 08:11:46 -0300

    Atualiza AppNavegacao e rotas_telas

--- MemoBrain/app/src/main/java/com/memobrain/memonow/navegacao/AppNavegacao.kt
+++ MemoBrain/app/src/main/java/com/memobrain/memonow/navegacao/AppNavegacao.kt
@@ -1,2 +1,41 @@
 package com.memobrain.memonow.navegacao
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+import com.memobrain.memonow.features.cadernos.CadernosTelas
+import com.memobrain.memonow.features.cadernos.CadernosTelas2
+import com.memobrain.memonow.features.login.LoginTela
+
+@Composable
+fun AppNavegacao() {
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = RotasTelas.LOGIN
+    ) {
+
+        composable(RotasTelas.LOGIN) {
+            LoginTela(
+                modifier = Modifier
+            )
+        }
+
+        composable(RotasTelas.CADERNOS) {
+            CadernosTelas(
+                modifier = Modifier
+            )
+        }
+
+        composable(RotasTelas.INICIO) {
+            CadernosTelas2(
+                modifier = Modifier
+            )
+        }
+    }
+}
---
+++ MemoBrain/app/src/main/java/com/memobrain/memonow/navegacao/rotas_telas.kt
@@ -0,0 +1,7 @@
+package com.memobrain.memonow.navegacao
+
+object RotasTelas {
+    const val LOGIN = "login"
+    const val CADERNOS = "cadernos"
+    const val INICIO = "inicio"
+}
