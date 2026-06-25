81c032554c364f26b866637de7acf80a5620ec17
AllysonAugusto <allysonfn9@hotmail.com>
2026-06-25 15:36:16 -0300

    feat: organiza rotas das telas

--- MemoBrain/app/src/main/java/com/memobrain/memonow/navegacao/rotas_telas.kt
+++ MemoBrain/app/src/main/java/com/memobrain/memonow/navegacao/rotas_telas.kt
@@ -1,19 +1,78 @@
 package com.memobrain.memonow.navegacao
 
-enum class RotasTelas {
-    INICIAL,
-    LOGIN,
-    REGISTRAR,
-    HOME,
-    INICIO_APP,
-    CADERNOS,
-    DETALHE_CADERNO,
-    EDITAR_CADERNO,
-    EDITAR_ARQUIVO,
-    CRIAR_CADERNO,
-    CRIAR_ARQUIVO,
-    CRIAR_PERGUNTA_ABERTA,
-    CRIAR_MULTIPLA_ESCOLHA,
-    REVISAR_ARQUIVO,
-    FLASHCARD_SUMMARY,
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed interface RotaTela : NavKey {
+    @Serializable
+    data object Inicial : RotaTela
+
+    @Serializable
+    data object Login : RotaTela
+
+    @Serializable
+    data object Registrar : RotaTela
+
+    @Serializable
+    data object InicioApp : RotaTela
+
+    @Serializable
+    data object Cadernos : RotaTela
+
+    @Serializable
+    data object Perfil : RotaTela
+
+    @Serializable
+    data class DetalheCaderno(
+        val cadernoId: String,
+        val nomeCaderno: String,
+    ) : RotaTela
+
+    @Serializable
+    data class EditarCaderno(
+        val cadernoId: String,
+    ) : RotaTela
+
+    @Serializable
+    data object CriarCaderno : RotaTela
+
+    @Serializable
+    data class CriarArquivo(
+        val cadernoId: String,
+    ) : RotaTela
+
+    @Serializable
+    data class EditarArquivo(
+        val cadernoId: String,
+        val arquivoId: String,
+    ) : RotaTela
+
+    @Serializable
+    data class CriarPerguntaAberta(
+        val cadernoId: String,
+        val arquivoId: String,
+    ) : RotaTela
+
+    @Serializable
+    data class CriarMultiplaEscolha(
+        val cadernoId: String,
+        val arquivoId: String,
+    ) : RotaTela
+
+    @Serializable
+    data class RevisarArquivo(
+        val cadernoId: String,
+        val cadernoTitulo: String,
+        val arquivoId: String,
+        val tituloArquivo: String,
+        val descricaoArquivo: String,
+    ) : RotaTela
+
+    @Serializable
+    data class ResumoFlashcard(
+        val acertos: Int,
+        val totalQuestoes: Int,
+        val tempoMillis: Long,
+    ) : RotaTela
 }
